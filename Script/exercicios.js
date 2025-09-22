0c1a9430cc52405861d9fca029a256b972a1cdb0
josematheus7 <math.filho1996@gmail.com>
2025-09-22 20:35:35 -0300

    ex18pt5

--- Script/exercicios.js
+++ Script/exercicios.js
@@ -307,7 +307,6 @@ function ex17_pt05(){
 //Ex 18 - Ordenação (sort): Dado o array [5, 1, 9, 3, 7], ordene-o em ordem crescente usando sort.
 function ex18_pt05(){
   const inputString = document.getElementById('txArray18').value;
-<<<<<<< HEAD
   let num = new Array()
   num = inputString.split(' ').map(str => str.trim())
   res = `${num.sort(function(a, b){
